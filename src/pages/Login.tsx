0cbba3e13fef8a93117e39267e04e801bc716c5d
nsalvador <nigelsalvador@gmail.com>
2020-09-23 21:59:06 -0400

    Added Login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,14 @@
-import React from "react";
+// eslint-disable-next-line
+import React from 'react';
 
-const Login = () => <h1>Login Page</h1>;
+const Login = () => (
+  <div className="box-layout">
+    <div className="box-layout__box">
+      <h1 className="box-layout__title">ReacTVity</h1>
+      <p>Subscribe to your favorite shows</p>
+      <button className="btn">Login with Google</button>
+    </div>
+  </div>
+);
 
 export default Login;
